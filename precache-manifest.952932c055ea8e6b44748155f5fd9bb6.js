d0d4c92fdc605f2b97a60a23b7722532889e9ea8
amkelv <kelvin.m@notjustevent.com>
2024-02-18 01:10:59 +0100

    init

---
+++ precache-manifest.952932c055ea8e6b44748155f5fd9bb6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a3fadfdcd022b53cc5ccf8a3e10a4b9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4195666e15c72e5e2ffe",
+    "url": "/static/css/main.2066abb9.chunk.css"
+  },
+  {
+    "revision": "a6659d6cbeb985af3eba",
+    "url": "/static/js/2.046f5434.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.046f5434.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4195666e15c72e5e2ffe",
+    "url": "/static/js/main.364baa53.chunk.js"
+  },
+  {
+    "revision": "4d77e6e00c5bfdfc89ac",
+    "url": "/static/js/runtime-main.eb0d3efc.js"
+  }
+]);
